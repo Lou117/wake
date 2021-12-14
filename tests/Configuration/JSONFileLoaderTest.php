8f159d8acb0dd77f3b538e7c410f7ed4b43f4eeb
Lou117 <lou117@users.noreply.github.com>
2021-12-14 16:08:57 +0100

    Removes Configuration\PHPArrayLoader class Adds PHPUnit Configuration\JSONFileLoaderTest

---
+++ tests/Configuration/JSONFileLoaderTest.php
@@ -0,0 +1,86 @@
+<?php
+
+use Monolog\Test\TestCase;
+use Lou117\Wake\Configuration\JSONFileLoader;
+
+class JSONFileLoaderTest extends TestCase
+{
+
+    protected string $validJSONFilepath;
+
+    protected string $invalidJSONFilepath;
+
+    protected string $notArrayJSONFilepath;
+
+    protected array $json = [
+        "test" => "test"
+    ];
+
+
+    public function setUp(): void
+    {
+        $this->validJSONFilepath = tempnam(sys_get_temp_dir(), "phpunitjsonfileloadertest");
+        file_put_contents($this->validJSONFilepath, json_encode([
+            "test" => "test"
+        ]));
+
+        $this->invalidJSONFilepath = tempnam(sys_get_temp_dir(), "phpunitjsonfileloadertest");
+        file_put_contents($this->invalidJSONFilepath, '["test" => "test"');
+
+        $this->notArrayJSONFilepath = tempnam(sys_get_temp_dir(), "phpunitjsonfileloadertest");
+        file_put_contents($this->notArrayJSONFilepath, "null");
+    }
+
+    public function tearDown(): void
+    {
+        unlink($this->validJSONFilepath);
+        unlink($this->invalidJSONFilepath);
+        unlink($this->notArrayJSONFilepath);
+    }
+
+    /**
+     * @covers \Lou117\Wake\Configuration\JSONFileLoader::load
+     * @return void
+     * @noinspection PhpDocMissingThrowsInspection
+     */
+    public function testLoad()
+    {
+        /** @noinspection PhpUnhandledExceptionInspection */
+        $this->assertSame(JSONFileLoader::load($this->validJSONFilepath), $this->json);
+    }
+
+    /**
+     * @covers \Lou117\Wake\Configuration\JSONFileLoader::load
+     * @return void
+     * @noinspection PhpDocMissingThrowsInspection
+     */
+    public function testLoadWithNotFoundFile()
+    {
+        $this->expectException(RuntimeException::class);
+        /** @noinspection PhpUnhandledExceptionInspection */
+        JSONFileLoader::load(uniqid());
+    }
+
+    /**
+     * @covers \Lou117\Wake\Configuration\JSONFileLoader::load
+     * @return void
+     * @noinspection PhpDocMissingThrowsInspection
+     */
+    public function testLoadWithInvalidJSON()
+    {
+        $this->expectException(JsonException::class);
+        JSONFileLoader::load($this->invalidJSONFilepath);
+    }
+
+    /**
+     * @covers \Lou117\Wake\Configuration\JSONFileLoader::load
+     * @return void
+     * @noinspection PhpDocMissingThrowsInspection
+     */
+    public function testLoadWithNotArrayJSON()
+    {
+        $this->expectException(RuntimeException::class);
+        /** @noinspection PhpUnhandledExceptionInspection */
+        JSONFileLoader::load($this->notArrayJSONFilepath);
+    }
+}
